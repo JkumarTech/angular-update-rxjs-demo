b64f17af6b9bd4437e0c6fc9246d24d5c7ba63dd
benlesh <ben@benlesh.com>
2018-04-18 10:59:16 -0700

    RxJS v 5 working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/interval';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/scan';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  data$ = Observable.interval(1000)
+    .scan((acc, n) => acc + n)
+    .map(n => `${new Date()}: ${n}`);
 }
